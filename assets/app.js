475d390d216ea724ab27859c74bef809e9b2ece3
TrainzCoder <prawira206jng@gmail.com>
2021-12-25 11:24:19 +0700

    Update CSS

--- assets/app.js
+++ assets/app.js
@@ -1,19 +1,24 @@
-sections = document.querySelectorAll("section[id]")
+const sections = document.querySelectorAll("section[id]");
 
-window.addEventListener("scroll", navHighlight)
 
-const navHighlight = () => {
-    let scrollY = window.pageYOffset
-    
-    sections.forEach(section => {
-        const sectionHeight = section.offsetHeight
-        const sectionTop = section.offSetTop - 50
-        const sectionId = section.getAttribute("id")
+window.addEventListener("scroll", navHighlighter);
 
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-            document.querySelector(".nav-link a[href=" + sectionId + "]").classList.add("active");
-        } else {
-            document.querySelector(".nav-link a[href=" + sectionId + "]").classList.remove("active");
-        }
-    });
+function navHighlighter() {
+
+  let scrollY = window.pageYOffset;
+  
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute("id");
+   
+    if (
+      scrollY > sectionTop &&
+      scrollY <= sectionTop + sectionHeight
+    ){
+      document.querySelector(".nav-link a[href*=" + sectionId + "]").parentElement.classList.add("active");
+    } else {
+      document.querySelector(".nav-link a[href*=" + sectionId + "]").parentElement.classList.remove("active");
+    }
+  });
 }
